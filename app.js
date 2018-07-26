b98eabce07576afac0cf03369f796bf9bfa1b677
David <jinwei.lin@gmail.com>
2018-07-26 16:03:52 +0800

    加上 Athena 查詢

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const maxmind = require('maxmind');
 const emptygif = require('emptygif');
 const moment = require('moment-timezone');
+const athena = require("athena-client");
 var AWS = require("aws-sdk");
 AWS.config.update({
   region: 'us-east-1'
@@ -16,12 +17,27 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+if ( typeof process.env.AWS_ACCESS_KEY_ID == 'undefined') {
+  console.log("please set : AWS_ACCESS_KEY_ID") ;
+  //process.exit();
+}
+if ( typeof process.env.AWS_SECRET_ACCESS_KEY == 'undefined') {
+  console.log("please set : AWS_SECRET_ACCESS_KEY") ;
+  //process.exit();
+}
+if ( typeof process.env.AWS_KINESIS_DELIVERY_STREAM_NAME == 'undefined') {
+  console.log("please set : AWS_KINESIS_DELIVERY_STREAM_NAME") ;
+  //process.exit();
+}
 var config = {
   aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
   aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
-  aws_kinesis_end_point: process.env.AWS_KINESIS_DELIVERY_STREAM_NAME
+  aws_kinesis_end_point: process.env.AWS_KINESIS_DELIVERY_STREAM_NAME,
+  region: 'us-east-1',
 };
-
+var clientConfig = {
+  bucketUri: 's3://user-accesslog/result'
+}
 // Enable CORS for all methods
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -29,6 +45,28 @@ app.use(function (req, res, next) {
   next();
 });
 
+//計算統計資料
+app.get('/dailyUpdate', function (req, res) {
+  var client = athena.createClient(clientConfig, config);
+  let sql = "select to_char(date,'yyyymmdd'), recid, array_join(array_agg(prod_no), ',', '') as prods from (" +
+  " select date, recid, prod_no from settour.user_accesslog where year = to_char(now()- interval '1' day,'yyyy') "+ 
+  " and month = to_char(now()- interval '1' day,'mm') and day = to_char(now()- interval '1' day,'dd') "+
+  " order by recid , date desc ) GROUP BY to_char(date,'yyyymmdd'), recid" ;
+  client.execute(sql).toPromise()
+    .then(function (data) {
+      console.log(data)
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(data));
+    }).catch(function (err) {
+      console.error(err)
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({
+        error: 0
+      }));
+    })
+});
+
+//使用者資料收集
 app.get('/cl', function (req, res) {
   if (Object.keys(req.query).length !== 0) {
     var record = {};
@@ -60,10 +98,10 @@ app.get('/cl', function (req, res) {
       record.hostname = req.query.hostname.toLowerCase();
     if (req.query.uri)
       record.uri = req.query.uri;
-    var ip = req.headers['x-forwarded-for'] ||
-      req.connection.remoteAddress ||
+    var ip = req.headers['x-forwarded-for'] || 
+      req.connection.remoteAddress || 
       req.socket.remoteAddress ||
-      req.connection.socket.remoteAddress; // IP
+      (req.connection.socket ? req.connection.socket.remoteAddress : ""); // IP
     record.ip = ip;
     record.userAgent = req.headers['user-agent'] || ''; //使用瀏覽器版本
     record.date = moment().tz("Asia/Taipei").format("YYYY-MM-DD HH:mm:ss"); //記錄時間
@@ -83,6 +121,11 @@ app.get('/cl', function (req, res) {
       }
     }
     if (Object.keys(record).length !== 0) {
+      emptygif.sendEmptyGif(req, res, {
+        'Content-Type': 'image/gif',
+        'Content-Length': emptygif.emptyGifBufferLength,
+        'Cache-Control': 'public, max-age=0' // or specify expiry to make sure it will call everytime
+      });
       console.log(JSON.stringify(record));
       var params = {
         DeliveryStreamName: config.aws_kinesis_end_point,
